56762adda536ce4acc1b61459b159059f00a2ce1
Biswajit Ghosh <ghoshbiswajit2406@gmail.com>
2022-11-07 20:58:59 +0530

    API modification

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
-import axios from 'axios'
-import React, {useState, useEffect} from 'react';
 import Coin from './Coin';
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
 
-  const[coins, setCoins]= useState([])
-  const[search, setSearch]=useState('')
-  useEffect(()=>{
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=500&page=1&sparkline=false').then(res=>{
-      setCoins(res.data);
-    }).catch(err =>{
-      console.log(err)
-    })
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false'
+      )
+      .then(res => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch(error => console.log(error));
   }, []);
 
-  const handleChange=e=>{
-    setSearch(e.target.value)
-  }
+  const handleChange = e => {
+    setSearch(e.target.value);
+  };
 
-  const filteredCoins= coins.filter(coin=>
+  const filteredCoins = coins.filter(coin =>
     coin.name.toLowerCase().includes(search.toLowerCase())
-  )
+  );
 
   return (
     <div className="coin-app">
@@ -41,3 +45,4 @@ function App() {
 }
 
 export default App;
+
